fd5ea8fbbad6e8024258b3dab1239936496c525f
Artur199324 <92278971+Artur199324@users.noreply.github.com>
2024-07-11 14:19:58 +0300

    Merge remote-tracking branch 'origin/main'

--- StartLibary/build.gradle.kts
+++ StartLibary/build.gradle.kts
@@ -50,7 +50,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.Artur199324"
                 artifactId = "StartLibrary"
-                version = "1.0.13"
+                version = "1.0.14"
 
                 pom {
                     name.set("StartLibrary")
